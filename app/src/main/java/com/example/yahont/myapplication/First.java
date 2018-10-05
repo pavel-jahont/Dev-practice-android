2243d6c4f1da359cee02cc177fc3f07b7774d11a
Pavel Jahont <pavel.yahont@actimind.com>
2018-10-05 15:56:31 +0300

    first commit in workBranch

---
+++ app/src/main/java/com/example/yahont/myapplication/First.java
@@ -0,0 +1,14 @@
+package com.example.yahont.myapplication;
+
+import android.util.Log;
+
+public class First {
+
+
+
+    public static void main() {
+        System.out.println("Print");
+
+        Log.d("TAG", "print message");
+    }
+}
